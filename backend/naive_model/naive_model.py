b41ffc073d3e86b1b4c1cb6dc98bef9a0210bacf
Jesse Warren <jfw224@lehigh.edu>
2025-04-14 02:59:37 -0400

    fixes for deployment

--- backend/naive_model/naive_model.py
+++ backend/naive_model/naive_model.py
@@ -1,8 +1,9 @@
 from data_preprocessor import preprocess_data
 from recommender import naive_recommend
+import os
 
 # Use relative path inside the project
-directory = "backend/yelp_data"
+directory = os.path.join(os.path.dirname(__file__), "../yelp_data")
 filename = "yelp_academic_dataset_business.json"
 
 restaurant_df = preprocess_data(directory, filename)
