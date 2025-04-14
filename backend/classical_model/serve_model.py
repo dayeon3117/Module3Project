c3f26ebe56aa19f3ddb0800329a20a7730b834e2
Jesse Warren <jfw224@lehigh.edu>
2025-04-14 13:46:15 -0400

    one more file path change

--- backend/classical_model/serve_model.py
+++ backend/classical_model/serve_model.py
@@ -7,7 +7,8 @@ def recommend_from_saved(query_idx=0, n=5):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     model_path = os.path.join(current_dir, "embeddings.npy")
     embeddings = np.load(model_path, allow_pickle=True)
-    df = pd.read_csv("backend/classical_model/metadata.csv")
+    metadata_path = os.path.join(current_dir, "metadata.csv")
+    df = pd.read_csv(metadata_path)
 
     recs = recommendations(embeddings, df, query_idx=query_idx, n=n)
 
